1fb358a183975c18c296a6c21c2283ba794883ab
Pongpatapee Peerapatanapokin <pongpatapee.peerapatanapokin@cummins.com>
2025-04-19 18:01:55 -0400

    feat: using blade directive

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -10,16 +10,15 @@
    <h2>Available ninjas</h2> 
 
    <ul>
-    <li>
-        <a href="/ninjas/{{$ninjas[0]['id']}}">
-            {{$ninjas[0]["name"]}}
-        </a>
-    </li>
-    <li>
-        <a href="/ninjas/{{$ninjas[1]['id']}}">
-            {{$ninjas[1]["name"]}}
-        </a>
-    </li>
+        @foreach($ninjas as $ninja)
+            <li>
+                <p> {{$ninja['name']}} </p>
+                <a href="/ninjas/{{$ninja['id']}}">
+                    View Details
+                </a>
+            </li> 
+        @endforeach
    </ul>
+
 </body>
 </html>
