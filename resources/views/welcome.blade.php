e0150d54b0c850f832fec28de0b0462d5d7d97b7
Pongpatapee Peerapatanapokin <pongpatapee.peerapatanapokin@cummins.com>
2025-04-19 20:37:43 -0400

    feat: add styling/tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
-<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel tutorial</title>
-</head>
-<body>
-    <h1>Laravel Tutorial</h1>
+    <title>Ninja Network</title>
 
-    <a href="/ninjas">Find ninjas</a>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="text-center px-8 py-12">
+    <h1>Welcome to the Ninja Network</h1>
+    <p>Click the button below to view the list of ninjas.</p>
+
+    <a href="/ninjas" class="btn mt-4 inline-block">
+        Find Ninjas!
+    </a>
 </body>
+
 </html>
